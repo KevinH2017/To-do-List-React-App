f0750a1933ff7a327755104a0aae1bf35da7d53b
KevinH <khui98@gmail.com>
2025-10-29 18:33:19 -0400

    Create Navbar.jsx

---
+++ src/Navbar.jsx
@@ -0,0 +1,29 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import TaskIcon from "@mui/icons-material/Task";
+
+export default function Navbar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <TaskIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            React Todo List App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
