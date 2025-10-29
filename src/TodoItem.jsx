1603e9914f79c2f8795b46be40157e83d8271309
KevinH <khui98@gmail.com>
2025-10-29 16:16:39 -0400

    Update TodoItem.jsx

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -2,6 +2,7 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import DeleteIcon from "@mui/icons-material/Delete";
 import Checkbox from "@mui/material/Checkbox";
 import IconButton from "@mui/material/IconButton";
 import CommentIcon from "@mui/icons-material/Comment";
@@ -14,7 +15,7 @@ export default function TodoItem({ todo, remove, toggle }) {
     <ListItem
       secondaryAction={
         <IconButton edge="end" aria-label="comments" onClick={remove}>
-          <CommentIcon />
+          <DeleteIcon />
         </IconButton>
       }
       disablePadding
